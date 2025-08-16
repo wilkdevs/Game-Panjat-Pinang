969a99e20a2ba72288dda74472aeb0d759814781
Wilkdevs <wilkdevscreative@gmail.com>
2025-08-17 03:00:24 +0700

    Update HomeController.php

--- app/Http/Controllers/User/HomeController.php
+++ app/Http/Controllers/User/HomeController.php
@@ -89,7 +89,7 @@ class HomeController extends Controller
 
         $voucher->load('gift');
 
-        dd($voucher->gift);
+        dd($voucher->gift());
 
         return view('/pages/user/index', [
             'title' => $title,
